20bd7d727bf3da7132cfb43de1e9429d1cae1f93
Shyamsundar Shrestha <ss.stha00@gmail.com>
2024-08-03 01:56:38 +0930

    (added) clerk authentication

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="flex min-h-screen w-full items-center justify-center">
