48d2eb7d5870d702a87efba3a0483635f071be26
Shyamsundar Shrestha <ss.stha00@gmail.com>
2024-08-03 02:32:57 +0930

    (updated) the font

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,15 +6,28 @@ import {
   UserButton,
 } from "@clerk/nextjs";
 import type { Metadata } from "next";
-import { Inter, Space_Grotesk} from "next/font/google";
+import { Inter, Space_Grotesk } from "next/font/google";
 import React from "react";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
 
 export const metadata: Metadata = {
   title: "sshoverflow",
   description: "A similar website to stackoverflow but better.",
+  icons: {
+    icon: "../public/assets/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -25,7 +38,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           <SignedOut>
             <SignInButton />
           </SignedOut>
