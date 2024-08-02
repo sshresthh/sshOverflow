42fb2beab87482f61622fa132cfa0a52ee264cf0
Shyamsundar Shrestha <ss.stha00@gmail.com>
2024-08-03 00:12:44 +0930

    (updated) home route

---
+++ app/(route)/(home)/page.tsx
@@ -0,0 +1,8 @@
+const Home = () => {
+  return (
+    <main className="flex min-h-screen w-full items-center justify-center">
+      <div>Home</div>
+    </main>
+  );
+};
+export default Home;
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="">
-      <h1>Hello World</h1>
-    </main>
-  );
-}
